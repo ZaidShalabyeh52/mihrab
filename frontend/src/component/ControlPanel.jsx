580d3d58c058771d476d08ab08ab7323ac9049bc
Shalabyeh <zbshalabiya24@cit.just.edu.jo>
2026-08-18 15:07:15 +0300

    added ControlPanel

--- frontend/src/component/ControlPanel.jsx
+++ frontend/src/component/ControlPanel.jsx
@@ -1,5 +1,16 @@
 function ControlPanel() {
-  return <main className="bg-gradient-bg w-2xl h-7 "> HI </main>;
+  return (
+    <section className="flex justify-end bg-gradient-bg w-full h-175 p-4 pr-6">
+      {/* Outside Border */}
+      <div className="flex items-center p-1 bg-gradient-gold rounded-xl h-fit">
+        <main className="flex items-start justify-center bg-gradient-bg rounded-[10px] w-100 h-125 p-2">
+          <div className="middle p-3.5 pb-4.5 m-2 font-[cairo] bg-gradient-gold text-white font-bold text-3xl rounded-xl shadow-md h-auto w-full">
+            لوحة التحكم
+          </div>
+        </main>
+      </div>
+    </section>
+  );
 }
 
 export default ControlPanel;
